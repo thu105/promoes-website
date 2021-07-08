14032b633b950226ab89828ba4118b7b54095dc9
Hein Thu <thu105@mail.chapman.edu>
2021-07-08 16:49:24 -0700

    Add Modules and react-router-dom

--- src/App.js
+++ src/App.js
@@ -1,5 +1,7 @@
 import logo from './logo.svg';
 import './App.css';
+import { BrowswerRouter, Route, Switch } from 'react-router-dom';
+import Modules from './components/Modules/Modules';
 
 function App() {
   return (
---
+++ src/components/Modules/Modules.js
@@ -0,0 +1,7 @@
+import React from 'react';
+
+export default function Modules() {
+    return(
+        <h2>Modules</h2>
+    );
+}
